c4f7ce310329130e0ad1af5762d4af73db04348d
Alejandro Junqui <ajunquit@gmail.com>
2025-08-08 19:18:59 -0500

    initial commit

---
+++ src/main/java/com/ajunquit/kvmd_api/KvmdApiApplication.java
@@ -0,0 +1,13 @@
+package com.ajunquit.kvmd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvmdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvmdApiApplication.class, args);
+	}
+
+}
