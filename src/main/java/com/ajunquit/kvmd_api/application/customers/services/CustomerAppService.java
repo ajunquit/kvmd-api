79ead679b0f953c31ea05e405826e66b03afabdc
Alejandro Junqui <ajunquit@gmail.com>
2025-08-09 13:22:57 -0500

    add CustomerAppService

--- src/main/java/com/ajunquit/kvmd_api/application/customers/services/CustomerAppService.java
+++ src/main/java/com/ajunquit/kvmd_api/application/customers/services/CustomerAppService.java
@@ -2,6 +2,8 @@ package com.ajunquit.kvmd_api.application.customers.services;
 
 import org.springframework.stereotype.Service;
 
+import com.ajunquit.kvmd_api.application.auth.services.JwtAppService;
+import com.ajunquit.kvmd_api.application.auth.services.UserAppService;
 import com.ajunquit.kvmd_api.application.customers.dtos.requests.CustomerRequest;
 import com.ajunquit.kvmd_api.application.customers.dtos.responses.CustomerResponse;
 import com.ajunquit.kvmd_api.application.customers.helper.CustomerHelper;
@@ -9,18 +11,17 @@ import com.ajunquit.kvmd_api.application.customers.mappers.CustomerMapper;
 import com.ajunquit.kvmd_api.domain.customer.entity.Customer;
 import com.ajunquit.kvmd_api.domain.customer.repository.CustomerRepository;
 
-@Service
-public class CustomerAppService {
-  
-  private final CustomerRepository _customerRepository;
+import lombok.RequiredArgsConstructor;
 
-  public CustomerAppService(CustomerRepository customerRepository ) {
-    _customerRepository = customerRepository;
-  }
+@Service
+@RequiredArgsConstructor
+public class CustomerAppService {  
+  private final CustomerRepository _customerRepository;
+  private final UserAppService _userAppService;
 
   public CustomerResponse createCustomer(CustomerRequest customerRequest) {
     Customer customer = CustomerMapper.toEntity(customerRequest);
-    CustomerHelper.setAuditFields(customer, "root");
+    CustomerHelper.setAuditFields(customer, _userAppService.getCurrentUsername());
     return CustomerMapper.toResponse(_customerRepository.save(customer));
   }
 }
