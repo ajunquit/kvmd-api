b225bc13fabfd572dc3ea75fb1cbcb3daa2f7f02
Alejandro Junqui <ajunquit@gmail.com>
2025-08-08 20:13:51 -0500

    set value to enum user status

--- src/main/java/com/ajunquit/kvmd_api/domain/auth/enums/UserStatus.java
+++ src/main/java/com/ajunquit/kvmd_api/domain/auth/enums/UserStatus.java
@@ -1,10 +1,28 @@
 package com.ajunquit.kvmd_api.domain.auth.enums;
 
 public enum UserStatus {  
-  CREATED,
-  ACTIVE,
-  INACTIVE,
-  DELETED,
-  BLOCKED,
-  UNKNOWN
+  CREATED(1),
+  ACTIVE(2),
+  INACTIVE(3),
+  BLOCKED(4),
+  UNKNOWN(0);
+
+  private final int code;
+
+  UserStatus(int code) {
+    this.code = code;
+  }
+
+  public int getCode() {
+    return code;
+  }
+  
+  public static UserStatus fromCode(int code) {
+    for (UserStatus status : UserStatus.values()) {
+      if (status.getCode() == code) {
+        return status;
+      }
+    }
+    return UNKNOWN;
+  }
 }
