c3147fd4284f052f9094303b2ed7b427e566b482
Alejandro Junqui <ajunquit@gmail.com>
2025-08-09 13:23:26 -0500

    update SecurityConfig

--- src/main/java/com/ajunquit/kvmd_api/presentation/auth/configs/SecurityConfig.java
+++ src/main/java/com/ajunquit/kvmd_api/presentation/auth/configs/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
       .csrf(csrf -> csrf.disable())
       .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth
-          .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+          .requestMatchers("/api/auth/register", "/api/auth/login", "/h2-console/**").permitAll()
           .anyRequest().authenticated()
       )
       .authenticationProvider(provider)
