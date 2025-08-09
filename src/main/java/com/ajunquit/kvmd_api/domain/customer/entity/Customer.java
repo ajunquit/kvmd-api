2f8ed4814cbfa4ecc128621d69f1d7b8a442c4f0
Alejandro Junqui <ajunquit@gmail.com>
2025-08-09 13:22:20 -0500

    add generateValue anotation to Customer.java entity

--- src/main/java/com/ajunquit/kvmd_api/domain/customer/entity/Customer.java
+++ src/main/java/com/ajunquit/kvmd_api/domain/customer/entity/Customer.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "customers")
 public class Customer {
     @Id
+    @GeneratedValue
     private UUID id;
 
     @Column(nullable = false)
